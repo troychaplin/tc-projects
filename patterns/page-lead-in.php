86e2642a28c2935ad6d284f8477506391a502faf
Troy Chaplin <troy.chaplin@gmail.com>
2025-11-16 16:29:25 -0500

    change: footer, homepage

---
+++ patterns/page-lead-in.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Title: Page Lead-In
+ * Slug: lumen/page-lead-in
+ * Categories: lumen-patterns
+ */
+?>
+
+<!-- wp:group {"tagName":"header","metadata":{"name":"Page Lead-in","patternName":"core/block/2444"},"align":"wide","className":"page-lead-in","style":{"spacing":{"margin":{"bottom":"0"},"blockGap":"var:preset|spacing|xs"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<header class="wp-block-group alignwide page-lead-in" style="margin-bottom:0"><!-- wp:heading {"level":1,"style":{"typography":{"lineHeight":"1.3"}},"fontSize":"custom-2"} -->
+<h1 class="wp-block-heading has-custom-2-font-size" style="line-height:1.3">Hi, I'm Troy Chaplin</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|emerald"}}}},"textColor":"emerald","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-emerald-color has-text-color has-link-color has-xx-large-font-size">Developer &amp; Designer</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-200"}}},"spacing":{"margin":{"top":"var:preset|spacing|md"}}},"textColor":"gray-200","fontSize":"x-large"} -->
+<p class="has-gray-200-color has-text-color has-link-color has-x-large-font-size" style="margin-top:var(--wp--preset--spacing--md)">Building for the web, writing about the process, and sharing knowledge across design and development.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"tagName":"section","metadata":{"name":"Stats Group"},"className":"stats-group","style":{"spacing":{"margin":{"top":"var:preset|spacing|xl"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<section class="wp-block-group stats-group" style="margin-top:var(--wp--preset--spacing--xl)"><!-- wp:group {"metadata":{"name":"Stat"},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-300"}}}},"textColor":"gray-300","fontSize":"x-small"} -->
+<p class="has-gray-300-color has-text-color has-link-color has-x-small-font-size">Focus</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">WordPress &amp; Frontend</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Stat"},"style":{"spacing":{"blockGap":"0","padding":{"right":"0","left":"var:preset|spacing|md"}},"border":{"right":{"width":"0px","style":"none"},"left":{"color":"var:preset|color|white-10","width":"1px"},"top":[],"bottom":[]}},"layout":{"type":"constrained"},"blockVisibility":{"controlSets":[{"id":1,"enable":true,"controls":{"screenSize":{"hideOnScreenSize":{"small":true}}}}]}} -->
+<div class="wp-block-group" style="border-right-style:none;border-right-width:0px;border-left-color:var(--wp--preset--color--white-10);border-left-width:1px;padding-right:0;padding-left:var(--wp--preset--spacing--md)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-300"}}}},"textColor":"gray-300","fontSize":"x-small"} -->
+<p class="has-gray-300-color has-text-color has-link-color has-x-small-font-size">Currently</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Building for Carleton</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Stat"},"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|md"}},"border":{"left":{"color":"var:preset|color|white-10","width":"1px"},"top":[],"right":[],"bottom":[]}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-left-color:var(--wp--preset--color--white-10);border-left-width:1px;padding-left:var(--wp--preset--spacing--md)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|gray-300"}}}},"textColor":"gray-300","fontSize":"x-small"} -->
+<p class="has-gray-300-color has-text-color has-link-color has-x-small-font-size">Contributing</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">WordPress Developer Blog</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></section>
+<!-- /wp:group --></header>
+<!-- /wp:group -->
