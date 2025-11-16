77d078f930fc6a8b5c4c52f0f2cd331e32636dfd
Troy Chaplin <troy.chaplin@gmail.com>
2025-11-16 11:12:47 -0500

    remove: pattern functions

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * TC Projects Theme Functions
+ * Lumen Theme Functions
  */
 
 // Exit if accessed directly
@@ -8,16 +8,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function tc_projects_add_editor_styles() {
+function lumen_add_editor_styles() {
 	add_theme_support( 'editor-styles' );
 	add_editor_style( 'assets/css/styles.css' );
 }
-add_action( 'after_setup_theme', 'tc_projects_add_editor_styles' );
+add_action( 'after_setup_theme', 'lumen_add_editor_styles' );
 
 /**
  * Enqueue custom styles
  */
-function tc_projects_enqueue_styles() {
+function lumen_enqueue_styles() {
 	wp_enqueue_style(
 		'lumen-styles',
 		get_template_directory_uri() . '/assets/css/styles.css',
@@ -25,23 +25,12 @@ function tc_projects_enqueue_styles() {
 		wp_get_theme()->get( 'Version' )
 	);
 }
-add_action( 'wp_enqueue_scripts', 'tc_projects_enqueue_styles' );
-
-/**
- * Register custom block patterns category
- */
-function tc_projects_register_pattern_categories() {
-	register_block_pattern_category(
-		'lumen',
-		array( 'label' => __( 'TC Projects', 'lumen' ) )
-	);
-}
-add_action( 'init', 'tc_projects_register_pattern_categories' );
+add_action( 'wp_enqueue_scripts', 'lumen_enqueue_styles' );
 
 /**
  * Register custom block styles
  */
-function tc_projects_register_block_styles() {
+function lumen_register_block_styles() {
 	// Stat item with gradient underline
 	register_block_style(
 		'core/tag-cloud',
@@ -51,5 +40,15 @@ function tc_projects_register_block_styles() {
 		)
 	);
 }
-add_action( 'init', 'tc_projects_register_block_styles' );
+add_action( 'init', 'lumen_register_block_styles' );
 
+/**
+ * Register custom block patterns category
+ */
+function lumen_register_pattern_categories() {
+    register_block_pattern_category(
+        'lumen-patterns',
+        array( 'label' => __( 'Lumen Patterns', 'lumen' ) )
+    );
+}
+add_action( 'init', 'lumen_register_pattern_categories' );
